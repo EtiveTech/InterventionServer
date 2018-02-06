6d99e7ce1ee187aa5cdfa52c57f4af771bed772b
Phil Crooks <coder.crooks@gmail.com>
2018-02-06 09:06:01 +0000

    logger path now in config file

--- api/lib/logger.php
+++ api/lib/logger.php
@@ -5,7 +5,12 @@
  * Date: 01/02/2018
  * Time: 08:57
  */
-include_once("../configuration.php");
+if (!defined(LOG_DIR)) {
+    if (file_exists('configuration.php'))
+        include_once 'configuration.php';
+    elseif (file_exists('../configuration.php'))
+        include_once '../configuration.php';
+}
 
 function logger($text) {
     file_put_contents(LOG_DIR,  $text . PHP_EOL, FILE_APPEND);
